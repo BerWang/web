76310dbdde9c5903d4c00854ad6bf9013a6df0f2
Mark Tyers <aa7401@coventry.ac.uk>
2017-02-21 14:39:44 +0000

    completed simple closure testing example

--- 06 Unit Testing/Web Client/js/shopping.js
+++ 06 Unit Testing/Web Client/js/shopping.js
@@ -1,59 +1,28 @@
-// shopping.js
-// This script calculates an order total.
 
-// Function called when the form is submitted.
-// Function performs the calculation and returns false.
-function calculate() {
-	'use strict'
-
-	// For storing the order total:
-	var total;
-
-	// Get references to the form values:
-	var quantity = document.getElementById('quantity').value;
-	var price = document.getElementById('price').value;
-	var tax = document.getElementById('tax').value;
-	var discount = document.getElementById('discount').value;
-
-	// Add validation here later!
-
-	// Calculate the initial total:
-	total = quantity * price;
-	console.log('total before tax: ' + total);
-
-	// Make the tax rate easier to use:
-	tax = tax / 100;
-	tax = tax + 1;
-
-	// Factor in the tax:
-	total = total * tax;
-	console.log("total after tax: " + total);
-
-	// Factor in the discount:
-	total = total - discount;
-	console.log("total after discount: " + total);
-
-	// Format the total to two decimal places:
-	total = total.toFixed(2);
-
-	// Display the total:
-	document.getElementById('total').value = total;
-
-	// Return false to prevent submission:
-	return false;
-
-} // End of calculate() function.
-
-// Function called when the window has been loaded.
-// Function needs to add an event listener to the form.
-function init() {
-	'use strict';
-
-	// Add an event listener to the form:
-	var theForm = document.getElementById('theForm');
-	theForm.onsubmit = calculate;
-
-} // End of init() function.
-
-// Assign an event listener to the window's load event:
-window.onload = init;
+function Shopping() {
+    var price = 0
+    var quantity = 0
+    var tax = 0
+    return {
+        setPrice: function(price) {
+            console.log('setPrice')
+            this.price = price
+        },
+        setQuantity: function(qty) {
+            if (!Number.isInteger(qty)) {
+                throw new Error('quantity needs to a a whole number')
+            }
+            this.quantity = qty
+        },
+        setTax: function(tax) {
+            this.tax = tax
+        },
+        getTotal: function() {
+            console.log('getTotal')
+            var total = Math.round(this.price * this.quantity * 100) / 100
+            taxFraction = this.tax/100 + 1
+            console.log(total)
+            return total
+        }
+    }
+}
---
+++ 06 Unit Testing/Web Client/spec/spec.js
@@ -0,0 +1,35 @@
+
+/* simple Jasmine test suite to illustrate how to unit test a closure */
+
+describe('calculating total costs', function() {
+	it('should calculate a simple total', function() {
+		try {
+			var shopping = new Shopping()
+			shopping.setPrice(10)
+			shopping.setQuantity(10)
+			console.log(shopping.getTotal())
+			expect(shopping.getTotal()).toBe(100)
+		} catch(err) {
+			// no errors should be thrown
+			expect(true).toBe(false)
+		}
+	})
+	
+	it('should calculate a simple total using a floating point price', function() {
+		var shopping = new Shopping()
+		shopping.setPrice(2.99)
+		shopping.setQuantity(10)
+		console.log(shopping.getTotal())
+		expect(shopping.getTotal()).toBe(29.90)
+	})
+
+	it('should throw an error if quantity is a float', function() {
+		try {
+			var shopping = new Shopping()
+			shopping.setQuantity(10.1)
+			expect(true).toBe(false)
+		} catch(err) {
+			expect(err.message).toBe('quantity needs to a a whole number')
+		}
+	})
+})
