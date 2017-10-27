74bad0ba437bfb1118f69ca40a170effc53a8d6c
Mark Tyers <aa7401@coventry.ac.uk>
2017-10-27 10:37:17 +0100

    updated syntax to ecma6

--- exercises/05_acceptance/cucumber/nodejs/features/step_definitions/browser_steps.js
+++ exercises/05_acceptance/cucumber/nodejs/features/step_definitions/browser_steps.js
@@ -1,22 +1,31 @@
 
 // features/step_definitions/browser_steps.js
+
+// page interactions use 'selenium'.
+
 const seleniumWebdriver = require('selenium-webdriver')
 const {defineSupportCode} = require('cucumber')
 
-defineSupportCode(function({Given, When, Then}) {
-  Given('I am on the Cucumber.js GitHub repository', function() {
-    return this.driver.get('https://github.com/cucumber/cucumber-js/tree/master')
-  })
+defineSupportCode(({Given, When, Then}) => {
+	Given('I am on the Cucumber.js GitHub repository', () => {
+		return this.driver.get('https://github.com/cucumber/cucumber-js/tree/master')
+	})
 
-  When('I click on {stringInDoubleQuotes}', function (text) {
-    return this.driver.findElement({linkText: text}).then(function(element) {
-      return element.click()
-    });
-  });
+	When('I click on {stringInDoubleQuotes}', text => {
+		return this.driver.findElement({linkText: text}).then( element => {
+			return element.click()
+		})
+	})
 
-  Then('I should see {stringInDoubleQuotes}', function (text) {
-    const xpath = "//*[contains(text(),'" + text + "')]"
-    const condition = seleniumWebdriver.until.elementLocated({xpath: xpath})
-    return this.driver.wait(condition, 5000)
-  })
+	When('I enter the text {stringInDoubleQuotes}', text => {
+		return this.driver.findElement({linkText: text}).then( element => {
+			return element.sendKeys(text)
+		})
+	})
+
+	Then('I should see {stringInDoubleQuotes}', text => {
+		const xpath = "//*[contains(text(),'" + text + "')]"
+		const condition = seleniumWebdriver.until.elementLocated({xpath: xpath})
+		return this.driver.wait(condition, 5000)
+	})
 })
